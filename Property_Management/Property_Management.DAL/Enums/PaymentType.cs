72e67ea989951d73198a28c7b37a7ffada04b482
kellyncodes <kellyrobot247@gmail.com>
2023-03-23 15:33:05 +0100

    fix: namespace sync

---
+++ Property_Management/Property_Management.DAL/Enums/PaymentType.cs
@@ -0,0 +1,39 @@
+﻿namespace Property_Management.DAL.Enums
+{
+    public enum PaymentType
+    {
+        Cash = 1,
+        Paypal,
+        CreditCard,
+        DebitCard,
+        EFT,
+        MobilePayment,
+        OnlinePay,
+        Check,
+        MoneyOrder,
+        WireTransfer,
+    }
+
+    public static partial class GetStringValue
+    {
+
+        public static string GetPaymentType(this PaymentType paymentType)
+        {
+            return paymentType switch
+            {
+                PaymentType.Cash => "Cash",
+                PaymentType.Paypal => "Paypal",
+                PaymentType.CreditCard => "Credit Card",
+                PaymentType.DebitCard => "Debit Card",
+                PaymentType.EFT => "Electronic Fund Transfer",
+                PaymentType.MobilePayment => "Mobile payment",
+                PaymentType.OnlinePay => "Online payment",
+                PaymentType.Check => "Check",
+                PaymentType.MoneyOrder => "Money Order",
+                PaymentType.WireTransfer => "Wire Transfer",
+                _ => string.Empty,
+            };
+        }
+    }
+}
+
--- Property_Management/Property_Management.DAL/Interfaces/IUnitOfWork.cs
+++ Property_Management/Property_Management.DAL/Interfaces/IUnitOfWork.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Property_Management.DAL.Interfaces
+﻿namespace Property_Management.DAL.Interfaces
 {
-        public interface IUnitOfWork : IDisposable
+    public interface IUnitOfWork : IDisposable
         {
             IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
 
