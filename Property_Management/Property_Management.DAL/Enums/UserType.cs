6b95738634337a64e094187d38b7805cccedd02a
kellyncodes <kellyrobot247@gmail.com>
2023-03-23 13:00:16 +0100

    feat: folder setup

---
+++ Property_Management/Property_Management.DAL/Enums/UserType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_Management.DAL.Enums
+{
+    public enum UserType
+    {
+        LandLord = 1,
+        Manager,
+        Tenant
+    }
+
+    public static partial class GetStringValue
+    {
+        public static string? UserTypeEnum(this UserType userType)
+        {
+            return userType switch
+            {
+                UserType.LandLord => "Landlord",
+                UserType.Manager => "Manager",
+                UserType.Tenant => "Tenant",
+                _ => null
+            };
+        }
+    }
+}
---
+++ Property_Management/Property_Management.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_Management.DAL.Interfaces
+{
+        public interface IUnitOfWork : IDisposable
+        {
+            IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+            int SaveChanges();
+
+            Task<int> SaveChangesAsync();
+        }
+
+        public interface IUnitOfWork<TContext> : IUnitOfWork
+        {
+        }
+}
