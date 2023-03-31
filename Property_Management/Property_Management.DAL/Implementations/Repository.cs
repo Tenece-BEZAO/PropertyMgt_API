6db3df2e3424ac25c5ac4e4edd9e64976802572b
Kelechi Amos <kellyrobot247@gmail.com>
2023-03-31 09:30:00 +0100

    Update Repository.cs

--- Property_Management/Property_Management.DAL/Implementations/Repository.cs
+++ Property_Management/Property_Management.DAL/Implementations/Repository.cs
@@ -530,21 +530,6 @@ namespace Property_Management.DAL.Implementation
             return query;
         }
 
-        public Task DeleteAsync(Task<Property> propertyToBeDeleted)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(IRepository<LandLord> landRepo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(Task<LandLord> landlord)
-        {
-            throw new NotImplementedException();
-        }
-
         //private IQueryable<T> ConstructQueryable(Expression<Func<T, bool>> predicate = null, string orderBy = null, int? skip = null, int? take = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
         //{
         //    try
